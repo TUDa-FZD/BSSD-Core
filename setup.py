9a87fba170d803cbf2e434cccaea4fdf405d0f81
Lippert, Moritz <moritz.lippert@tu-darmstadt.de>
2023-10-31 09:22:21 +0100

    Initial commit to main

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='bssd',
+   version='0.1',
+   description='BSSD Library',
+   author='Eric Krämer',
+   author_email='eric.kraemer@stud.tu-darmstadt.de',
+   packages=['bssd'],
+   install_requires=['osmium'],
+)
